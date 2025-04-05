d41345a2c4164946f744439302ed128aefb6cebb
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-05 17:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model)
 	this.topSpeed=topSpeed
 }
+
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar
 SportsCar.prototype.getTopSpeed=function(){
 	return this.topSpeed
 }
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar
-
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
