dadd67fe26cea0bc68e9b07b4c9deeb7f8f2bf2b
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-05 16:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make
+		this.model=model
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel=function(){
+		let str=this.make + " "+this.model;
+		return str;
+	}
 
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed
+}
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
